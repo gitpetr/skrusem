bbb61219592acf0dc340d982be6c417557de9487
gitpetr <pkhilenko@yandex.ru>
2016-05-24 00:02:45 +0700

    asset ske

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require ckeditor/override
+//*= require ckeditor/ckeditor
 //= require ckeditor/init
 //= require turbolinks
 //= require_tree .
